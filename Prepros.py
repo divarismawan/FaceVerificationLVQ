460b6a3cb649b5cdb7ce5ba7b5b9d770146b670e
FX504 <divarismawan.20@gmail.com>
2019-04-23 16:24:06 +0800

    resize

--- Prepros.py
+++ Prepros.py
@@ -15,9 +15,10 @@ for category in classes:
     for img in os.listdir(path):
         img_array = cv2.imread(os.path.join(path, img))
 
+        # Add contras image
         contrast_img = cv2.addWeighted(img_array, 1.5, np.zeros(img_array.shape, img_array.dtype), 0, 0)
 
-        # Face Detection
+        # Face Detection library
         face_casecade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
@@ -27,7 +28,6 @@ for category in classes:
             cv2.rectangle(contrast_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             roi_gray = contrast_img[y:y + h, x:x + w]
             roi_color = contrast_img[y:y + h, x:x + w]
-
 
         # # crop - get center
         # height, width = roi_color.shape[0:2]
@@ -47,17 +47,21 @@ for category in classes:
         abs_dst = cv2.convertScaleAbs(lapl)
 
         dim = (300, 300)
-        resized = cv2.resize(abs_dst, dim, interpolation=cv2.INTER_AREA)
+        resized_lapl = cv2.resize(abs_dst, dim, interpolation=cv2.INTER_AREA)
+        resized_gray = cv2.resize(gray, dim, interpolation=cv2.INTER_AREA)
 
-        # detected_faces = detect_faces(abs_dst)
-
-        # cv2.imshow("grayscale",contrast_img)
-        # cv2.imshow("gaussian",gaussian)
-        cv2.imshow("laplacian", resized)
-        # cv2.imshow("hasil laplacian", detected_faces)
+        cv2.imshow("grayscale",gray)
+        cv2.imshow("laplacian", resized_lapl)
 
         cv2.waitKey(0)
 
-        hasil = url_class + category + '/ROI_{}_{}.jpg'.format(category, img)
-        cv2.imwrite(hasil, resized)
+        # Save Image
+        # ROI with Laplacian
+        hasil = url_class + category + '/ROI_LAPL_{}_{}.jpg'.format(category, img)
+        cv2.imwrite(hasil, resized_lapl)
         print("{} gambar {} tersimpan".format(category,img))
+
+        # ROI grayscale
+        hasil = url_class + category + '/ROI_GRAY_{}_{}.jpg'.format(category, img)
+        cv2.imwrite(hasil, resized_gray)
+        print("{} gambar {} tersimpan".format(category, img))
