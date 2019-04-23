c0415a23c37d58a94f9766e3e1fb50b4c492d0db
FX504 <divarismawan.20@gmail.com>
2019-04-23 15:16:25 +0800

    save edit

--- Prepros.py
+++ Prepros.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+import numpy as np
+from PIL import Image
 
 url_class = 'Dataset/Face/'
 classes = os.listdir(url_class)
@@ -13,35 +15,48 @@ for category in classes:
     for img in os.listdir(path):
         img_array = cv2.imread(os.path.join(path, img))
 
-        grey = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
+        contrast_img = cv2.addWeighted(img_array, 1.5, np.zeros(img_array.shape, img_array.dtype), 0, 0)
 
         # Face Detection
         face_casecade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-        faces = face_casecade.detectMultiScale(img_array, 1.3, 1)
+        faces = face_casecade.detectMultiScale(contrast_img, 1.3, 1)
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(grey, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = grey[y:y + h, x:x + w]
-            roi_color = grey[y:y + h, x:x + w]
+            cv2.rectangle(contrast_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = contrast_img[y:y + h, x:x + w]
+            roi_color = contrast_img[y:y + h, x:x + w]
 
-        # cv2.imshow('img', grey)
 
-        # cv2.imshow('roi',roi_color)
+        # # crop - get center
+        # height, width = roi_color.shape[0:2]
+        # startRow = int(height * .15)
+        # startCol = int(width * .15)
+        # endRow = int(height * .90)
+        # endCol = int(width * .90)
+        # croppedImage = roi_color[startRow:endRow, startCol:endCol]
+
 
         # Preprocessing
-        gaussian = cv2.GaussianBlur(roi_gray, (3, 3), 1)
+        gaussian = cv2.GaussianBlur(roi_color, (3, 3), 3)
 
-        lapl = cv2.Laplacian(gaussian, cv2.CV_16S, 10)
+        gray = cv2.cvtColor(gaussian, cv2.COLOR_BGR2GRAY)
+
+
+        lapl = cv2.Laplacian(gray, cv2.CV_16S, 5)
 
         abs_dst = cv2.convertScaleAbs(lapl)
 
         # detected_faces = detect_faces(abs_dst)
 
-        # cv2.imshow("grayscale",img_array)
-        # cv2.imshow("hasil grayscale",gaussian)
-        cv2.imshow("hasil laplacian", abs_dst)
+        # cv2.imshow("grayscale",contrast_img)
+        # cv2.imshow("gaussian",gaussian)
+        # cv2.imshow("laplacian", abs_dst)
         # cv2.imshow("hasil laplacian", detected_faces)
 
-        cv2.waitKey(0)
+        # cv2.waitKey(0)
+
+        hasil = url_class + category + '/ROI_{}_{}.jpg'.format(category, img)
+        cv2.imwrite(hasil, lapl)
+        print("{} gambar {} tersimpan".format(category,img))
